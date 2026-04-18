2aa2113964e343760e412f4cadd0a0773caf7d46
Akash070301 <akashrk0703@gmail.com>
2026-04-18 21:31:12 +0530

    Client side for Login and Register done

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,7 +1,48 @@
-export default function Login(){
-    return(
-        <>
-        <h1>Login Page</h1>
-        </>
-    )
+import { Link } from "react-router-dom";
+import "../App.css";
+import { Form, Button, Input } from "antd";
+export default function Login() {
+  return (
+    <>
+      <header className="App-header">
+        <main className="main-area mw-500 text-center px-3">
+          <section className="left-section">
+            <h1>Login to BookMyShow</h1>
+          </section>
+          <section className="right-section">
+            <Form layout="vertical">
+              <Form.Item
+                label="Email"
+                htmlFor="email"
+                className="d-block"
+                rules={[{ required: true, message: "Please enter email" }]}
+              >
+                <Input id="email" type="email" placeholder="Enter your email" />
+              </Form.Item>
+              <Form.Item
+                label="Password"
+                htmlFor="password"
+                className="d-block"
+                rules={[{ required: true, message: "Please enter password" }]}
+              >
+                <Input
+                  id="password"
+                  type="text"
+                  placeholder="Enter your password"
+                />
+              </Form.Item>
+              <Form.Item className="d-block" htmlType="submit">
+                <Button type="primary" block className="button-primary">
+                  Login
+                </Button>
+              </Form.Item>
+              <p>
+                Not registered? Sign up <Link to="/register">Register</Link>
+              </p>
+            </Form>
+          </section>
+        </main>
+      </header>
+    </>
+  );
 }
--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -1,7 +1,55 @@
-export default function Register(){
-    return(
-        <>
-        <h1>Register Page</h1>
-        </>
-    )
+import { Link } from "react-router-dom";
+import { Form, Button, Input } from "antd";
+export default function Register() {
+  return (
+    <>
+      <header className="App-header">
+        <main className="main-area mw-500 text-center px-3">
+          <section className="left-section">
+            <h1>Register to BookMyShow</h1>
+          </section>
+          <section className="right-section">
+            <Form layout="vertical">
+              <Form.Item
+                label="Name"
+                htmlFor="name"
+                className="d-block"
+                rules={[{ required: true, message: "Please enter name" }]}
+              >
+                <Input id="name" type="text" placeholder="Enter your name" />
+              </Form.Item>
+              <Form.Item
+                label="Email"
+                htmlFor="email"
+                className="d-block"
+                rules={[{ required: true, message: "Please enter email" }]}
+              >
+                <Input id="email" type="email" placeholder="Enter your email" />
+              </Form.Item>
+              <Form.Item
+                label="Password"
+                htmlFor="password"
+                className="d-block"
+                rules={[{ required: true, message: "Please enter password" }]}
+              >
+                <Input
+                  id="password"
+                  type="text"
+                  placeholder="Enter your password"
+                />
+              </Form.Item>
+              <Form.Item className="d-block" htmlType="submit">
+                <Button type="primary" block className="button-primary">
+                  Register
+                </Button>
+              </Form.Item>
+              <p>
+                Already registered ? <Link to="/login">Login here</Link>
+              </p>
+            </Form>
+          </section>
+        </main>
+      </header>
+    </>
+  );
 }
